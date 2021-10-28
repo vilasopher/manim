7378c8c9e2b8b0bb48a7d37bf93c8ec846586664
Vilas Winstein <vilas.win@gmail.com>
2021-10-28 20:36:07 +0200

    made lots of progress, still need to make canopy binary tree look nice

--- binary_tree_limit/animations.py
+++ binary_tree_limit/animations.py
@@ -3,12 +3,12 @@ import networkx as nx
 import random
 import highlight_ball as hb
 import solarized as sol
+import binary_tree as bt
 
-class TestScene(Scene):
+class BoxScene(Scene):
     def construct(self):
         self.camera.background_color = sol.BASE3
 
-        nxgraph = nx.balanced_tree(2,5)
         nxgraph = nx.grid_2d_graph(15,15)
 
         vconf = { v : {'fill_color' : sol.BASE02} for v in nxgraph.nodes() }
@@ -30,3 +30,31 @@ class TestScene(Scene):
                 self.play(hb.HighlightBall(g,v,2,node_highlight_color=sol.BLUE,edge_highlight_color=sol.BLUE))
             self.wait(0.5)
 
+class TreeScene(Scene):
+    def construct(self):
+        self.camera.background_color = sol.BASE3
+
+        nxgraph = nx.balanced_tree(2,8)
+        vconf = {'fill_color' : sol.BASE02}
+        econf = {'stroke_color' : sol.BASE01}
+
+
+        nxgraph = nx.balanced_tree(2,6)
+        g = Graph.from_networkx(nxgraph, vertex_config=vconf, edge_config=econf, layout=bt.binary_tree_layout(8))
+        self.add(g)
+        self.play(g.animate.change_layout(bt.canopy_tree_layout(6, height=2)))
+        self.wait()
+        
+        return
+
+        self.play(Create(g))
+
+        for i in range(4):
+            depth = i + 1
+            nodes, edges = bt.binary_tree_layer(depth)
+            self.play(g.animate.add_vertices(*nodes, positions=bt.binary_tree_layout(depth)),
+                      g.animate.add_edges(*edges))
+            self.wait(0.25)
+
+        self.play(g.animate.change_layout(bt.canopy_tree_layout(5)))
+        self.wait()
