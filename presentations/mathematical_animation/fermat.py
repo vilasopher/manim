892c4e1c2423bb5b145d5076bd3ffa6762a70ace
Vilas Winstein <vilas.win@gmail.com>
2022-02-02 13:05:40 +0100

    did more fermat

--- presentations/mathematical_animation/fermat.py
+++ presentations/mathematical_animation/fermat.py
@@ -1,7 +1,11 @@
 from manim import *
 
 def colorfunction(t, freq = 0.1, **kwargs):
-    return interpolate_color(BLUE, RED, (1 + np.cos(freq * PI * t)) / 2)
+    return interpolate_color(
+            PURE_BLUE,
+            PURE_RED, 
+            (1 + np.cos(freq * PI * t)) / 2
+    )
 
 def colored_plot(f, **kwargs):
     curve = ParametricFunction(f, **kwargs)
@@ -18,7 +22,7 @@ def colored_plot(f, **kwargs):
 def gamma(a, t):
     return [
         - 5 * np.sqrt(a**2 + 1) * np.cos(t),
-        5 * (np.sqrt(a**2 + 1) * np.sin(t) - a) - 3,
+        5 * (np.sqrt(a**2 + 1) * np.sin(t) - a) - 2,
         0 
     ]
 
@@ -29,29 +33,44 @@ def gamma_range(a, step = 0.005):
         step
     ]
 
-class Test(Scene):
+class Fermat(Scene):
     def construct(self):
+
+        p = Dot([-5, -2, 0], z_index = 1)
+        q = Dot([5, -2, 0], z_index = 1)
+
+        static_arc = ParametricFunction(
+            lambda t : gamma(0.2, t),
+            t_range = gamma_range(0.2),
+            stroke_width = 5,
+            stroke_color = GRAY,
+            z_index = -1
+        )
+
+        self.add(p, q, static_arc)
         
         a = ValueTracker(0.1)
         
-        arc = colored_plot(
+        moving_arc = colored_plot(
             lambda t : gamma(a.get_value(), t),
             t_range = gamma_range(a.get_value()),
-            stroke_width = 10
+            stroke_width = 10,
+            z_index = 0
         )
 
-        arc.add_updater(
+        moving_arc.add_updater(
             lambda s : s.become(
                 colored_plot(
                     lambda t : gamma(a.get_value(), t),
                     t_range = gamma_range(a.get_value()),
-                    stroke_width = 10
+                    stroke_width = 10,
+                    z_index = 0
                 )
             )
         )
 
-        self.add(arc)
+        self.add(moving_arc)
         
-        for _ in range(3):
+        for _ in range(1):
             self.play(a.animate.set_value(0.3))
             self.play(a.animate.set_value(0.1))
