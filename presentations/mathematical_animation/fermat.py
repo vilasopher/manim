3b6622ea5567311be83af0e9ee3b365be5e1490f
Vilas Winstein <vilas.win@gmail.com>
2022-02-02 17:49:04 +0100

    figured more stuff out

--- presentations/mathematical_animation/fermat.py
+++ presentations/mathematical_animation/fermat.py
@@ -8,7 +8,13 @@ def colorfunction(t, freq = 0.1, **kwargs):
     )
 
 def colored_plot(f, stroke_width = 10, z_index = 0, **kwargs):
-    curve = ParametricFunction(f, **kwargs)
+    curve = ParametricFunction(
+        f,
+        stroke_width = stroke_width,
+        z_index = z_index,
+        **kwargs
+    )
+
     pieces = CurvesAsSubmobjects(curve)
 
     colors = [
@@ -17,9 +23,12 @@ def colored_plot(f, stroke_width = 10, z_index = 0, **kwargs):
     ]
 
     pieces.set_color_by_gradient(*colors)
+    
     return pieces
 
-def gamma(a, t):
+def gamma(a_inv, t):
+    a = 1 / a_inv if a_inv != 0 else 2 ** 16
+
     if a >= 0:
         return [
             - 5 * np.sqrt(a**2 + 1) * np.cos(t),
@@ -29,11 +38,13 @@ def gamma(a, t):
     else:
         return [
             - 5 * np.sqrt(a**2 + 1) * np.cos(t),
-            - 5 * (np.sqrt(a**2 + 1) * np.sin(t) - a) - 2,
+            - 5 * (np.sqrt(a**2 + 1) * np.sin(t) + a) - 2,
             0 
         ]
 
-def gamma_range(a, step = 0.005):
+def gamma_range(a_inv, step = 0.005):
+    a = 1 / a_inv if a_inv != 0 else 2 ** 16
+    
     return [ 
         np.arccos(1 / np.sqrt(a**2 + 1)),
         PI - np.arccos(1 / np.sqrt(a**2 + 1)),
@@ -46,46 +57,57 @@ class Fermat(Scene):
         p = Dot([-5, -2, 0], z_index = 1)
         q = Dot([5, -2, 0], z_index = 1)
 
-        static_arc = ParametricFunction(
-            lambda t : gamma(0.2, t),
-            t_range = gamma_range(0.2),
+        static_arc_nonstationary = ParametricFunction(
+            lambda t : gamma(5, t),
+            t_range = gamma_range(5),
             stroke_width = 5,
             stroke_color = GRAY,
             z_index = -1
         )
 
-        self.add(p, q, static_arc)
-        
-        a = ValueTracker(0.1)
-        
-        moving_arc = colored_plot(
-            lambda t : gamma(a.get_value(), t),
-            t_range = gamma_range(a.get_value())
+        static_arc_stationary = ParametricFunction(
+            lambda t : [t, -2, 0],
+            t_range = [-5, 5, 0.1],
+            stroke_width = 5,
+            stroke_color = GRAY,
+            z_index = -1
         )
+        
+        bs = [ ValueTracker(-0.1), ValueTracker(4) ]
+        
+        moving_arcs = [
+            colored_plot(
+                lambda t : gamma(b[i].get_value(), t),
+                t_range = gamma_range(b[i].get_value())
+            )
+            for i in range(2)
+        ]
 
-        moving_arc.add_updater(
-            lambda s : s.become(
-                colored_plot(
-                    lambda t : gamma(a.get_value(), t),
-                    t_range = gamma_range(a.get_value())
+        for i in range(2):
+            moving_arcs[i].add_updater(
+                lambda s : s.become(
+                    colored_plot(
+                        lambda t : gamma(b[i].get_value(), t),
+                        t_range = gamma_range(b[i].get_value())
+                    )
                 )
             )
+
+        self.add(
+            p,
+            q,
+            static_arc_nonstationary,
+            static_arc_stationary,
+            moving_arcs[0],
+            moving_arcs[1]
         )
 
-        self.add(moving_arc)
-
-        self.play(
-            a.animate.set_value(100),
-            rate_func = rate_functions.rush_into
-        )
-        a.set_value(-100)
-        self.play(
-            a.animate.set_value(-0.1),
-            rate_func = rate_functions.rush_from
-        )
-
-        return
-        
         for _ in range(1):
-            self.play(a.animate.set_value(0.3))
-            self.play(a.animate.set_value(0.1))
+            self.play(
+                b[0].animate.set_value(0.1),
+                b[1].animate.set_value(6)
+            )
+            self.play(
+                b[0].animate.set_value(-0.1),
+                b[1].animate.set_value(4)
+            )
