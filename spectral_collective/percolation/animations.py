bb24954137bb30a40ad7cf1271f006d440789413
Vilas Winstein <vilas.win@gmail.com>
2022-02-25 16:13:19 +0100

    found some good parameters
    Former-commit-id: 2d062a1e822517f2044ef4c49a7eba4428beb21b

--- spectral_collective/percolation/animations.py
+++ spectral_collective/percolation/animations.py
@@ -2,12 +2,14 @@ from manim import *
 import grid as gr
 from more_graphs import HPGraph
 import networkx as nx
-from random import random
+from random import random, seed
 import solarized as sol
+
+seed(1)
 
 class Percolate(Scene):
     def construct(self):
-        nodes, edges = gr.grid_nodes_edges(15, 8)
+        nodes, edges = gr.grid_nodes_edges(24, 14)
 
         nxgraph = nx.Graph()
         nxgraph.add_nodes_from(nodes)
@@ -15,7 +17,7 @@ class Percolate(Scene):
 
         g = HPGraph.from_networkx(
             nxgraph,
-            layout=gr.grid_layout(15, 8),
+            layout=gr.grid_layout(24, 14, scale=0.3),
             vertex_config = sol.LIGHT_VERTEX_CONFIG,
             edge_config = sol.LIGHT_EDGE_CONFIG
         )
@@ -23,8 +25,8 @@ class Percolate(Scene):
         self.add(g)
         self.wait()
 
-        self.play(g.animate.percolate(0.6))
+        self.play(g.animate.percolate(0.505))
         self.wait()
 
-        self.play(g.animate.dramatically_highlight_ball((0,0), 3))
+        self.play(g.animate.dramatically_highlight_ball((0,0)))
         self.wait()
