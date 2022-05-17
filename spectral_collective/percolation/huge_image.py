24c49f4b79023372c05bf578f39e5c2fdf7700b0
Vilas Winstein <vilas.win@gmail.com>
2022-05-17 21:29:00 +0200

    huge
    Former-commit-id: f2250d0f38e65583c54e2625cd7ae32df212f6d0

--- spectral_collective/percolation/huge_image.py
+++ spectral_collective/percolation/huge_image.py
@@ -1,7 +1,8 @@
 import numpy as np
-from random import random, seed
+from random import random, seed, randint
+from PIL import Image
 
-shape = (5400, 9600)
+shape = (1080, 1920)
 p = 0.5
 seed(0)
 
@@ -24,6 +25,33 @@ for n in range(numverts):
 
 for n in range(numverts):
     if data[n,4] == -1:
-        # TODO: THIS PART
+        color = randint(0, 2**24)
 
+        print(n)
+        print(color)
 
+        to_visit = [n]
+        while len(to_visit) > 0:
+            cur = to_visit.pop()
+            data[cur,4] = color
+
+            for d in range(4):
+                if data[cur,d] != -1 and data[data[cur,d],4] == -1:
+                    to_visit.append(data[cur,d])
+
+def int_to_rgb(col):
+    return np.uint8([
+        col // 2**16,
+        (col % 2**16) // 2**8,
+        col % 2**8
+        ])
+
+print('rendering...')
+
+pixels = np.uint8(
+    [ [ int_to_rgb(data[i + j * shape[0], 4])
+        for j in range(shape[1]) ]
+        for i in range(shape[0]) ])
+
+img = Image.fromarray(pixels, mode='RGB')
+img.save("huge/test.png")
