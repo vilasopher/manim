1d43b3668ce26121223eeb8c62f4775c403d688c
Vilas Winstein <vilas.win@gmail.com>
2022-07-28 16:44:17 -0400

    sectoin 15

---
+++ spectral_collective/percolation/section_15.py
@@ -0,0 +1,212 @@
+from manim import *
+import grid as gr
+import solarized as sol
+import networkx as nx
+from more_graphs import PercolatingGraph, HPGraph, HPGrid
+from glitch import Glitch, GlitchEdges, GlitchPercolate
+from translucent_box import TranslucentBox
+from duality import Duality, convert_edge
+import random
+
+# (8,5)   0.95
+
+class DualityScene(Scene):
+    def construct(self):
+        g = Duality()
+
+        self.wait()
+
+        self.play(FadeIn(g.primal))
+
+        self.wait(5.5)
+        
+        self.play(*(FadeIn(g.dual[v]) for v in g.dual.vertices))
+        
+        self.wait(3)
+
+        self.play(*(Create(g.dual.edges[e]) for e in g.dual.edges))
+
+        self.wait(5.5)
+
+        self.play(FadeOut(g.dual))
+
+        self.wait(2)
+
+        self.play(FadeOut(g.primal), FadeIn(g.dual))
+
+        self.wait(2)
+
+        self.play(FadeIn(g.primal))
+
+        self.wait(6)
+
+        edict = { e : True if random.random() < 0.5 else False for e in g.primal.edges }
+        elist = list(g.primal.edges)
+        random.shuffle(elist)
+
+        for v in g.primal.vertices:
+            g.primal[v].set(z_index = 2)
+
+        for v in g.dual.vertices:
+            g.dual[v].set(z_index = 2)
+
+        self.play(
+            LaggedStart(
+                *(
+                    AnimationGroup(
+                        Glitch(g.primal.edges[e], intensity = 0.03, out = edict[e]),
+                        Glitch(g.dual.edges[convert_edge(*e)], intensity = 0.03, out = not edict[e]),
+                        run_time = 0.5
+                    ) for e in elist
+                )
+            ), run_time = 8
+        )
+
+        self.wait(1.5)
+
+        self.play(FadeOut(g.primal))
+
+        self.wait(0.75)
+
+        self.play(
+            GlitchEdges(g.dual),
+            run_time=0.25
+        )
+
+class PrimalGlitchTransition(Scene):
+    def construct(self):
+        g = Duality()
+        self.play(GlitchEdges(g.primal, intensity=0.03), run_time=0.5)
+
+class DualGlitchTransition(Scene):
+    def construct(self):
+        g = Duality()
+        self.play(GlitchEdges(g.dual, intensity=0.03), run_time=0.5)
+
+class BothGlitchTransition(Scene):
+    def construct(self):
+        g = Duality()
+        self.play(
+            GlitchEdges(g.primal, intensity=0.03),
+            GlitchEdges(g.dual, intensity=0.03),
+            run_time=0.5
+        )
+
+class DualPercolateHi(Scene):
+    def construct(self):
+        g = Duality()
+        g.percolate(0.75)
+        self.play(GlitchEdges(g.dual, intensity=0.03), run_time=0.25)
+        self.wait(2.5)
+        self.play(GlitchEdges(g.dual, intensity=0.03), run_time=0.25)
+
+class DualPercolateLo(Scene):
+    def construct(self):
+        g = Duality()
+        g.percolate(0.25)
+        self.play(GlitchEdges(g.dual, intensity=0.03), run_time=0.25)
+        self.wait(1.75)
+        self.play(FadeIn(g.primal))
+        self.wait(1.75)
+        self.play(
+            GlitchEdges(g.primal, intensity=0.03),
+            GlitchEdges(g.dual, intensity=0.03), 
+            run_time=0.25
+        )
+
+class PercolateBoth(Scene):
+    def construct(self):
+        g = Duality()
+        g.percolate()
+        self.play(
+            GlitchEdges(g.primal, intensity=0.03),
+            GlitchEdges(g.dual, intensity=0.03), 
+            run_time=0.25
+        )
+        self.wait(2.75)
+        self.play(FadeOut(g.dual))
+        self.wait(0.25)
+        self.play(
+            GlitchEdges(g.primal, intensity=0.03),
+            run_time=0.25
+        )
+
+class PercolatePrimal(Scene):
+    def construct(self):
+        g = Duality()
+        g.percolate()
+
+        self.play(GlitchEdges(g.primal, intensity=0.03), run_time=0.25)
+        self.wait(1.75)
+        self.play(
+            FadeIn(g.dual),
+            FadeOut(g.primal)
+        )
+        self.wait(0.25)
+        self.play(
+            GlitchEdges(g.dual, intensity=0.03),
+            run_time=0.25
+        )
+
+class PercolateDual(Scene):
+    def construct(self):
+        g = Duality()
+        g.percolate()
+
+        self.play(GlitchEdges(g.dual, intensity=0.03), run_time=0.25)
+        self.wait(1.75)
+        self.play(FadeIn(g.primal))
+        self.wait(0.25)
+        self.play(
+            GlitchEdges(g.primal, intensity=0.03),
+            GlitchEdges(g.dual, intensity=0.03),
+            run_time=0.25
+        )
+
+class PercolateBothLonger1(Scene):
+    def construct(self):
+        g = Duality()
+        g.percolate()
+
+        self.play(
+            GlitchEdges(g.primal, intensity=0.03),
+            GlitchEdges(g.dual, intensity=0.03),
+            run_time=0.25
+        )
+
+        self.wait(3)
+
+        self.play(
+            GlitchEdges(g.primal, intensity=0.03),
+            GlitchEdges(g.dual, intensity=0.03),
+            run_time=0.25
+        )
+
+class PercolateBothLonger2(PercolateBothLonger1):
+    pass
+
+class GlitchZoomOutA(MovingCameraScene):
+    def construct(self):
+        g = Duality((24, 14), 0.95)
+
+        self.play(
+            self.camera.frame.animate.scale(0.95/0.3),
+            GlitchEdges(g.primal, intensity=0.03),
+            GlitchEdges(g.dual, intensity=0.03),
+            run_time = 0.5
+        )
+
+
+class GlitchZoomOutB(MovingCameraScene):
+    def construct(self):
+        g = self.Duality((24, 14), 0.3)
+
+        self.camera.frame.save_state()
+        self.camera.frame.scale(0.3/0.95)
+
+        self.play(
+            self.camera.frame.animate.restore(),
+            GlitchEdges(g.primal, intensity=0.04),
+            GlitchEdges(g.dual, intensity=0.04),
+            run_time = 0.5
+        )
---
+++ spectral_collective/percolation/section_15_transparencies.py
@@ -0,0 +1,87 @@
+from manim import *
+import solarized as sol
+from translucent_box import TranslucentBox
+from value_slider import CriticalValueSlider
+import math
+
+config.background_opacity = 0
+
+lemma = MathTex(
+    r'\textbf{Lemma: } {{ p_c }} < 1.',
+    color = sol.BASE03
+).set_color_by_tex(r'p_c', sol.BLUE).move_to(4.75 * LEFT + 3.25 * UP)
+
+primal = MathTex(
+    r'\text{Primal} {{ \text{ Grid} }}',
+    color = sol.BASE03,
+    font_size = 60
+)
+
+dual = MathTex(
+    r'\text{Dual} {{ \text{ Grid} }}',
+    color = sol.BASE03,
+    font_size = 60
+)
+
+class Lemma(Scene):
+    def construct(self):
+        self.add(TranslucentBox(lemma), lemma)
+
+class PrimalDualTex(Scene):
+    def construct(self):
+        self.play(Write(primal))
+        self.wait(1)
+        self.play(TransformMatchingTex(primal, dual))
+        self.wait(10)
+
+class PrimalDualBox(Scene):
+    def construct(self):
+        tbox = TranslucentBox(primal)
+        self.add(tbox)
+
+        self.wait()
+        self.wait(1)
+        self.play(Transform(tbox, TranslucentBox(dual)))
+        self.wait(10)
+
+class SliderMid(Scene):
+    def construct(self):
+        cvs = CriticalValueSlider(0.5)
+        self.add(cvs)
+
+class SliderHi(Scene):
+    def construct(self):
+        cvs = CriticalValueSlider(0.75)
+        self.add(cvs)
+
+class SliderLo(Scene):
+    def construct(self):
+        cvs = CriticalValueSlider(0.25)
+        self.add(cvs)
+
+class SliderMidToHi(Scene):
+    def construct(self):
+        p = ValueTracker(0.5)
+        cvs = CriticalValueSlider(p.get_value())
+        cvs.add_updater(lambda s : s.set_p(p.get_value()))
+        self.add(cvs)
+
+        self.play(p.animate.set_value(0.75), run_time=0.5)
+
+class SliderHiToLo(Scene):
+    def construct(self):
+        p = ValueTracker(0.75)
+        cvs = CriticalValueSlider(p.get_value())
+        cvs.add_updater(lambda s : s.set_p(p.get_value()))
+        self.add(cvs)
+
+        self.play(p.animate.set_value(0.25), run_time=0.5)
+
+class SliderLoToMid(Scene):
+    def construct(self):
+        p = ValueTracker(0.25)
+        cvs = CriticalValueSlider(p.get_value())
+        cvs.add_updater(lambda s : s.set_p(p.get_value()))
+        self.add(cvs)
+
+        self.play(p.animate.set_value(0.5), run_time=0.5)
