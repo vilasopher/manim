2e13aaf83e454e96ae57abe9b0906e280de72056
Vilas Winstein <vilas.win@gmail.com>
2023-08-01 09:49:35 -0700

    got an idea for the crystal

--- spectral_collective/LIS/conclusion.py
+++ spectral_collective/LIS/conclusion.py
@@ -9,6 +9,7 @@ data = np.zeros((RES[0]+2, RES[1]+2*BUFF), dtype=np.uint8) # 80 extra columns on
 data[-1] = np.ones(RES[1]+2*BUFF) # bottom row starts with a block
 clocks = ra.exponential(1/24, size=RES[1]+2*BUFF-2) # never try the first or last column to avoid runtime errors
 pixels = np.full((*RES,3), [253,246,227], dtype=np.uint8)
+pppixels = np.copy(pixels)
 
 def hsv_to_rgb(h, s, v):
     if s:
@@ -44,11 +45,26 @@ def add_block(c, t):
         else: 
             r += 1
 
+def post_process(pixels):
+    pppixels = np.copy(pixels)
+
+    for c in range(RES[1]):
+        r = 0
+        while r < RES[0]:
+            if data[r+1,c+BUFF] == 1:
+                for i in range(min(8, RES[0]-r)):
+                    pppixels[r+i,c] = np.uint8([0,43,54])
+                r = RES[0]
+            else:
+                r += 1
+            
+    return pppixels
+
 time = 0
 dt = 1/(60*360)
 ticks = np.full(RES[1]+2*BUFF-2, dt)
 
-for frame in range(3000):
+for frame in range(2700):
     for _ in range(360):
         time += dt
         clocks -= ticks
@@ -59,6 +75,8 @@ for frame in range(3000):
                 add_block(c+1, time)
                 clocks[c] = ra.exponential(1/24)
 
-    img = Image.fromarray(pixels)
+    pppixels = post_process(pixels)
+
+    img = Image.fromarray(pppixels)
     img.save(rf"data/images/frame{frame}.png") 
     print(rf"generated frame {frame}!")
