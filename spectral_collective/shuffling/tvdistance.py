62e379cc73e2032eb0ad3498bb895319c83e1880
Vilas Winstein <vilas.win@gmail.com>
2024-05-08 13:01:18 -0700

    going to ttoust

--- spectral_collective/shuffling/tvdistance.py
+++ spectral_collective/shuffling/tvdistance.py
@@ -4,8 +4,19 @@ import solarized as sol
 class TVDefinition(Scene):
     def construct(self):
         def1text = MathTex(
-            r'd_{\mathrm{TV}}(\mu, \lambda) = \frac{1}{2} \sum_{x \in \Omega} |\mu(x) - \lambda(x)|',
-            color=sol.BASE3
+            r'\textbf{Definition 1: } \mathrm{d_{TV}}(\mu, \lambda) = \frac{1}{2} \sum_{x \in \Omega} |\mu(x) - \lambda(x)|',
+            color=sol.BASE03
+        )
+        def2text = MathTex(
+            r'\textbf{Definition 2: } \mathrm{d_{TV}}(\mu,\lambda) = \max_{A \subseteq \Omega} |\mu(A) - \lambda(A)|',
+            color=sol.BASE03
+        )
+        def3text = MathTex(
+            r'\textbf{Definition 3: } \mathrm{d_{TV}}(\mu, \lambda) = \min_{\substack{X \sim \mu \\ Y \sim \lambda}} \mathbb{P}[X \neq Y]',
+            color=sol.BASE03
         )
 
-        self.add(def1text)
+        def1text.next_to(def2text, UP).align_to(def2text, LEFT)
+        def3text.next_to(def2text, DOWN).align_to(def2text, LEFT)
+
+        self.add(def1text, def2text, def3text)
